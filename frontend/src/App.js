d98bd359673cf8b4e33697da5269bcbe5f58975d
Sawan <sawan8492@gmail.com>
2023-01-26 20:00:07 +0530

    clarifai api format change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
+    console.log(data);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
